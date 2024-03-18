dffb9b4b715c106752bc921f1c9e6da8277b8a8e
Pawan Gautam <pawangtm2419@gmail.com>
2024-03-18 10:28:04 +0530

    ILL for item label

--- src/app/masters/item-label/item-label.component.ts
+++ src/app/masters/item-label/item-label.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { environment } from 'src/environments/environment';
 
 @Component({
   selector: 'app-item-label',
@@ -6,5 +7,10 @@ import { Component } from '@angular/core';
   styleUrls: ['./item-label.component.css']
 })
 export class ItemLabelComponent {
+  searchText: any;
+  itemLocationLabelData: any = [];
+  limit: any = environment.pageLimit;
+  p: number = 1;
+  isNotFound: boolean = false;
 
 }
