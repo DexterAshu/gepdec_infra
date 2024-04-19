6f9d4ede73f02e53bd80da389fa78275c8bf6ca9
Pawan Gautam <pawangtm2419@gmail.com>
2024-04-19 15:10:10 +0530

    L1 schedule and Material issuance API done

--- src/app/documentation/tender-document/l1-schedule/l1-schedule.component.ts
+++ src/app/documentation/tender-document/l1-schedule/l1-schedule.component.ts
@@ -1,7 +1,7 @@
-import { Component, OnInit, HostListener } from '@angular/core';
+import { Component, OnInit, HostListener, ElementRef } from '@angular/core';
 import { FormBuilder, FormGroup, Validators } from '@angular/forms';
 import { environment } from 'src/environments/environment';
-import { ApiService, AlertService } from 'src/app/_services';
+import { ApiService, AlertService, SharedService } from 'src/app/_services';
 import * as XLSX from 'xlsx';
 import * as FileSaver from 'file-saver';
 
@@ -12,6 +12,7 @@ import * as FileSaver from 'file-saver';
 })
 
 export class L1ScheduleComponent {
+  today: any = new Date();
   documentForm!: FormGroup;
   attachment: File[] = [];
   isSubmitted = false;
@@ -32,24 +33,28 @@ export class L1ScheduleComponent {
   discardeddata: any;
   isExcelDownloadData: boolean = true;
 
-  constructor(private fb: FormBuilder, private apiService: ApiService, private alertService: AlertService) { }
+  constructor(private fb: FormBuilder, private apiService: ApiService, private alertService: AlertService, private sharedService: SharedService, private elementRef: ElementRef) { }
 
   ngOnInit() {
     this.getData();
     this.documentForm = this.fb.group({
-      company_id: ['', Validators.required],
-      tender_ref_no: ['', Validators.required],
-      total_tenure: ['', Validators.required],
-      start_date: ['', Validators.required],
-      end_date: ['', Validators.required],
-      publish_date: ['', Validators.required],
-      attachment: ['', Validators.required],
-      description: ['']
+      company_id: [null, Validators.required],
+      tender_id: [null, Validators.required],
+      start_date: [null, Validators.required],
+      end_date: [null, Validators.required],
+      total_tenure: [null, Validators.required],
+      attachment: [null],
+      description: [null]
     });
   }
 
+  ngAfterViewInit() {
+    this.sharedService.initializeTooltips(this.elementRef);
+  }
+
   getData() {
-    this.apiService.getDocListData().subscribe((res: any) => {
+    const apiLink = `/document/api/v1/getL1ScheduleList`;
+    this.apiService.getData(apiLink).subscribe((res: any) => {
       if (res.status === 200) {
         this.docListData = res.result;
       } else {
@@ -107,16 +112,6 @@ export class L1ScheduleComponent {
     }
   }
 
-  removeSelectedFile(index: number) {
-    this.listOfFiles.splice(index, 1);
-    this.attachment.splice(index, 1);
-  }
-
-  @HostListener('window:resize', ['$event'])
-  onResize(event: Event) {
-    this.tableHeight = `${window.innerHeight * 0.65}px`;
-  }
-
   isOpen: boolean = false;
 
   toggleDropdown() {
@@ -125,12 +120,6 @@ export class L1ScheduleComponent {
 
   get f() {
     return this.documentForm.controls;
-  }
-
-  downloadPdf() {
-    const pdfUrl = './assets/tamplate/country_bulkload_template_file.xlsx';
-    const pdfName = 'country_bulkload_template_file.xlsx';
-    FileSaver.saveAs(pdfUrl, pdfName);
   }
 
   download(): void {
@@ -144,27 +133,25 @@ export class L1ScheduleComponent {
     for (let i = 0; i < this.attachment.length; i++) {
       formData.append('attachment', this.attachment[i]);
     }
-    formData.append('tender_title', this.documentForm.value.tender_title);
-    formData.append('tender_ref_no', this.documentForm.value.tender_ref_no);
-    formData.append('total_tenure', this.documentForm.value.total_tenure);
-    formData.append('start_date', this.documentForm.value.start_date);
-    formData.append('end_date', this.documentForm.value.end_date);
-    formData.append('utility', this.documentForm.value.utility);
-    formData.append('publish_date', this.documentForm.value.publish_date);
+    formData.append('tender_id', this.documentForm.value.tender_id);
+    formData.append('company_id', this.documentForm.value.company_id);
+    formData.append('l1_start_date', this.documentForm.value.start_date);
+    formData.append('l1_end_date', this.documentForm.value.end_date);
+    formData.append('tender_completion_period', this.documentForm.value.total_tenure);
     formData.append('description', this.documentForm.value.description);
-    // this.apiService.createDocuments(formData).subscribe((res: any) => {
-    //   if (res.status == 200) {
-    //     this.documentForm.reset();
-    //     this.alertService.success(res.message);
-    //   } else {
-    //     this.alertService.warning(res.message);
-    //   }
-    //   this.isSubmitted = false;
-    //   document.getElementById('cancel')?.click();
-    // }),
-    // (error: any) => {
-    //   this.isSubmitted = false;
-    //   this.alertService.error(`Error: ${error.message}`);
-    // }
+    this.apiService.l1ScheduleUpload(formData).subscribe((res: any) => {
+      if (res.status == 200) {
+        this.documentForm.reset();
+        this.alertService.success(res.message);
+      } else {
+        this.alertService.warning(res.message);
+      }
+      this.isSubmitted = false;
+      document.getElementById('cancel')?.click();
+    }),
+    (error: any) => {
+      this.isSubmitted = false;
+      this.alertService.error(`Error: ${error.message}`);
+    }
   }
 }
