7651b63c5ad88c9797254572559d1af5d0ad44e2
DexterAshu <getaashish01@gmail.com>
2024-06-09 17:05:40 +0530

    costing approval done

--- src/app/presales/presales-biding/costing-approval/costing-approval.component.ts
+++ src/app/presales/presales-biding/costing-approval/costing-approval.component.ts
@@ -57,7 +57,6 @@ export class CostingApprovalComponent {
   selectedRow: any;
   itemList: any;
   totalDirectCost: number = 0;
-  marginPer: number = 12;
   overallDirectCost: number = 0;
   overallInDirectCost: number = 0;
   totalMargin: number = 0;
@@ -67,6 +66,15 @@ export class CostingApprovalComponent {
   profitPer: number = 0;
   tenderID: any;
   locationArray: any = [];
+  targetDirect: number = 5;
+  marginDirect: number = 12;
+  marginIndirect: number = 12;
+  indirectInnerBody: any = [];
+  indirectClicked: boolean = false;
+  directInnerBody: any = [];
+  directClicked: boolean = false;
+  isDirectAvl: boolean = false;
+  isIndirectAvl: boolean = false;
 
 
   constructor(
@@ -91,6 +99,7 @@ export class CostingApprovalComponent {
       tenderstatus_id: ['', Validators.required],
       working_notes: ['', Validators.required],
       audit_trail: [null],
+      marginPer: [null],
     });
 
     this.getTenderData();
@@ -105,22 +114,7 @@ export class CostingApprovalComponent {
     this.locationArray = row.tender_location;
   }
 
-  validateInput() {
-   
-    if (this.marginPer === null || this.marginPer === undefined) {
-      this.marginPer = 0;
-    }  else if(this.marginPer < 0) {
-      this.marginPer = 0;
-    } else if(this.marginPer > 100) {
-      this.marginPer = 100;
-    }
-
-    this.totalProjectCost = this.overallDirectCost + this.overallInDirectCost;
-    this.totalMargin = (this.totalProjectCost * this.marginPer) / 100;
-    this.totalProjectCostWithMargin = this.totalProjectCost + this.totalMargin;
-    this.totalProfit = +this.rowData?.ecv - this.totalProjectCostWithMargin;
-    this.profitPer = (this.totalProfit / +this.rowData?.ecv) * 100;
-  }
+  get f() { return this.form.controls; }
 
   getBOQItemList(data: any) {
     this.totalDirectCost = 0;
@@ -128,19 +122,6 @@ export class CostingApprovalComponent {
     this.itemList = data;
     this.itemList?.items.map((item: any) => this.totalDirectCost += item?.total_basic_value);
   }
-
-  //  getDetails(data:any){
-  //   if (data) {
-  //     if (this.userData && this.userData.rolename === "Administrator") {
-  //       this.router.navigate(['presales/presales-biding/management-approval',data]);
-  //     } else {
-  //       this.router.navigate(['/presales/presales-biding/data-capture',data]);
-  //     }
-  //     this.router.navigate(['/presales/presales-biding/data-capture']);
-  //     this.button = 'Update';
-  //     this.update = true;
-  //   } else {}
-  // }
 
   getDetails(data: any) {
     console.log(data);
@@ -154,21 +135,81 @@ export class CostingApprovalComponent {
   }
 
   rowListData(row: any) {
+    debugger
+    this.directClicked = false;
+    this.indirectClicked = false;
     this.rowData = row;
     this.tenderID = row.tender_id;
+
     if (this.rowData.directCost.length > 0 && this.rowData?.indirectCost.length > 0) {
-      this.rowData.directCost[0]?.items?.map((el: any) => {
-        this.overallDirectCost += +el?.total_freight_with_GST_value;
+
+      this.rowData.directCost[0]?.boq?.forEach((el: any) => {
+        // this.overallDirectCost += +el?.total_freight_with_GST_value;
+        el?.items?.forEach((data: any) => {
+
+          if (data.margin_direct == "0.00") {
+            this.isDirectAvl = false;
+            data.marginDirect = this.marginDirect;
+            data.marginAmt = +data?.totalWithFreightWithGST * (this.marginDirect / 100);
+            data.afterMarginAmt = +data?.totalWithFreightWithGST + +data?.marginAmt;
+          } else {
+            this.isDirectAvl = true;
+            data.marginAmt = +data?.totalWithFreightWithGST * (data?.margin_direct / 100);
+            data.afterMarginAmt = +data?.totalWithFreightWithGST + +data?.margin_direct;
+          }
+
+          if (data.target_direct == "0.00") {
+            data.targetDirect = this.targetDirect;
+            data.targetUnitPrice = +data?.unit_price * (1 - (this.targetDirect / 100));
+          } else {
+            data.targetUnitPrice = +data?.unit_price * (1 - (data?.target_direct / 100));
+          }
+          
+          data?.childItemList?.forEach((child: any) => {
+            if (child.margin_direct == "0.00") {
+              child.marginDirect = this.marginDirect;
+              child.marginAmt = +child?.totalWithFreightWithGST * (this.marginDirect / 100);
+              child.afterMarginAmt = +child?.totalWithFreightWithGST + +child?.marginAmt;
+            } else {
+              child.marginAmt = +child?.totalWithFreightWithGST * (child?.margin_direct / 100);
+              child.afterMarginAmt = +child?.totalWithFreightWithGST + +child?.margin_direct;
+            }
+
+            if (child.target_direct == "0.00") {
+              child.targetDirect = this.targetDirect;
+              child.targetUnitPrice = +child?.unit_price * (1 - (this.targetDirect / 100));
+            } else {
+              child.targetUnitPrice = +child?.unit_price * (1 - (child?.target_direct / 100));
+            }
+          });
+
+        });
       });
-  
-      this.rowData.indirectCost?.map((el: any) => {
+
+      this.rowData.indirectCost?.forEach((el: any) => {
         this.overallInDirectCost += +el?.all_total;
+        // let tMargin = 0;
+        // let tAmtWithMargin = 0;
+        el?.records?.forEach((data: any) => {
+          if (data.margin_indirect == "0.00") {
+            this.isIndirectAvl = false;
+            data.marginIndirect = this.marginIndirect;
+            data.marginAmt = +data?.total * (this.marginIndirect / 100);
+            data.afterMarginAmt = +data?.total + +data?.marginAmt;
+          } else {
+            this.isIndirectAvl = true;
+            data.marginAmt = +data?.total * (data?.margin_indirect / 100);
+            data.afterMarginAmt = +data?.total + +data?.margin_indirect;
+          }
+          // tMargin += +data?.marginAmt
+          // tAmtWithMargin += +data?.afterMarginAmt
+        });
+        // el.totalMargin += +tMargin
+        // el.totalAfterMarginAmt += +tAmtWithMargin
       });
     }
 
     this.reqList = this.rowData.requestStatus;
-    console.log(this.reqList);
-
     var roleD = this.roleStatusData.filter((res: any) => {
       return res.tender_id == this.rowData.tender_id;
     })
@@ -176,8 +217,193 @@ export class CostingApprovalComponent {
 
   }
 
+  validateInput() {
+    debugger
+    if (this.marginIndirect === null || this.marginIndirect === undefined) {
+      this.marginIndirect = 0;
+    } else if (this.marginIndirect < 0) {
+      this.marginIndirect = 0;
+    } else if (this.marginIndirect > 100) {
+      this.marginIndirect = 100;
+    }
 
-  get f() { return this.form.controls; }
+    this.rowData.indirectCost?.forEach((el: any) => {
+      el?.records?.forEach((data: any) => {
+        data.marginAmt = +data?.total * (this.marginIndirect / 100);
+        data.afterMarginAmt = +data?.total + +data?.marginAmt;
+
+        // el.totalMargin += +data.marginAmt
+        // el.totalAfterMarginAmt += +data.afterMarginAmt
+        data.marginIndirect = this.marginIndirect;
+      });
+    });
+
+    // this.totalProjectCost = this.overallDirectCost + this.overallInDirectCost;
+    // this.totalMargin = (this.totalProjectCost * this.marginPer) / 100;
+    // this.totalProjectCostWithMargin = this.totalProjectCost + this.totalMargin;
+    // this.totalProfit = +this.rowData?.ecv - this.totalProjectCostWithMargin;
+    // this.profitPer = (this.totalProfit / +this.rowData?.ecv) * 100;
+  }
+  
+  validateInputDirect(val:string) {
+    debugger
+
+    if(val == 'margin') {
+      if (this.marginDirect === null || this.marginDirect === undefined) {
+        this.marginDirect = 0;
+      } else if (this.marginDirect < 0) {
+        this.marginDirect = 0;
+      } else if (this.marginDirect > 100) {
+        this.marginDirect = 100;
+      }
+  
+      this.rowData.directCost[0].boq?.forEach((el: any) => {
+        el?.items?.forEach((data: any) => {
+          data.marginDirect = this.marginDirect;
+          data.marginAmt = +data?.totalWithFreightWithGST * (data?.marginDirect / 100);
+          data.afterMarginAmt = +data?.totalWithFreightWithGST + +data?.marginAmt;
+  
+          data?.childItemList?.forEach((child: any) => {
+            child.marginDirect = this.marginDirect;
+            child.marginAmt = +child?.totalWithFreightWithGST * (child?.marginDirect / 100);
+            child.afterMarginAmt = +child?.totalWithFreightWithGST + +child?.marginAmt;
+          });
+  
+        });
+      });
+    } else {
+      if (this.targetDirect === null || this.targetDirect === undefined) {
+        this.targetDirect = 0;
+      } else if (this.targetDirect < 0) {
+        this.targetDirect = 0;
+      } else if (this.targetDirect > 100) {
+        this.targetDirect = 100;
+      }
+  
+      this.rowData.directCost[0].boq?.forEach((el: any) => {
+        el?.items?.forEach((data: any) => {
+          data.targetDirect = this.targetDirect;
+          data.targetUnitPrice = +data?.unit_price * (1 - (data?.targetDirect / 100));
+  
+          data?.childItemList?.forEach((child: any) => {
+            child.targetDirect = this.targetDirect;
+            child.targetUnitPrice = +child?.unit_price * (1 - (child?.targetDirect / 100));
+          });
+  
+        });
+      });
+    }
+
+    // this.totalProjectCost = this.overallDirectCost + this.overallInDirectCost;
+    // this.totalMargin = (this.totalProjectCost * this.marginPer) / 100;
+    // this.totalProjectCostWithMargin = this.totalProjectCost + this.totalMargin;
+    // this.totalProfit = +this.rowData?.ecv - this.totalProjectCostWithMargin;
+    // this.profitPer = (this.totalProfit / +this.rowData?.ecv) * 100;
+  }
+
+  recalculatePrices(item: any, val:string) {
+    debugger
+    if(val == 'direct') {
+      if (item.marginDirect === null || item.marginDirect === undefined) {
+        item.marginDirect = 0;
+      } else if (item.marginDirect < 0) {
+        item.marginDirect = 0;
+      } else if (item.marginDirect > 100) {
+        item.marginDirect = 100;
+      }
+      item.marginAmt = (+item.totalWithFreightWithGST * item.marginDirect) / 100;
+      item.afterMarginAmt = +item.totalWithFreightWithGST + +item.marginAmt;
+      item.targetUnitPrice = +item?.unit_price * (1 - (item?.targetDirect / 100));
+      
+    } else {
+      if (item.marginIndirect === null || item.marginIndirect === undefined) {
+        item.marginIndirect = 0;
+      } else if (item.marginIndirect < 0) {
+        item.marginIndirect = 0;
+      } else if (item.marginIndirect > 100) {
+        item.marginIndirect = 100;
+      }
+      item.marginAmt = (+item.total * item.marginIndirect) / 100;
+      item.afterMarginAmt = +item.total + +item.marginAmt;
+    }
+
+  }
+
+  saveDirectCosting() {
+    debugger
+    this.directInnerBody = [];
+    this.directClicked = true
+    this.rowData.directCost[0].boq?.forEach((el: any) => {
+      el?.items?.forEach((data: any) => {
+
+        let itemData = {
+          boqitem_id: data?.boqitem_id,
+          margin_direct: data?.marginDirect,
+          target_direct: data?.targetDirect,
+        }
+        this.directInnerBody.push(itemData);
+        
+        data?.childItemList?.forEach((child: any) => {
+          let childData = {
+            boqitem_id: child?.boqitem_id,
+            margin_direct: child?.marginDirect,
+            target_direct: child?.targetDirect,
+          }
+          this.directInnerBody.push(childData);
+        });
+
+      });
+    });
+    
+    let directBody = {
+      boq_id: null,
+      directCost: this.directInnerBody
+    }
+
+    let apiLink = "/boq/api/v1/updateBOQMargin";
+    this.apiService.putData(apiLink, directBody).subscribe((res: any) => {
+      if (res.status === 200) {
+        this.getTenderData();
+        this.alertService.success("Margin and Target added in Direct Costing");
+      } else {
+        this.alertService.error(res.message);
+      }
+    }, (error: any) => {
+      this.alertService.error("Error: Unknown Error!");
+    });
+  }
+  
+  saveIndirectCosting() {
+    debugger
+    this.indirectInnerBody = [];
+    this.indirectClicked = true
+    this.rowData.indirectCost?.forEach((el: any) => {
+      el?.records?.forEach((data: any) => {
+        let innerData = {
+          indirectcost_id: data.indirectcost_id,
+          margin_indirect: data.marginIndirect,
+        }
+        this.indirectInnerBody.push(innerData);
+      });
+    });
+    let indirectBody = {
+      tender_id: this.rowData.tender_id,
+      indirectcost: this.indirectInnerBody
+    }
+
+    let apiLink = "/costing/api/v1/updateTenderIndirectCost";
+    this.apiService.putData(apiLink, indirectBody).subscribe((res: any) => {
+      if (res.status === 200) {
+        this.getTenderData();
+        this.alertService.success("Margin added in Indirect Costing");
+      } else {
+        this.alertService.error(res.message);
+      }
+    }, (error: any) => {
+      this.alertService.error("Error: Unknown Error!");
+    });
+  }
+
 
   getTenderData() {
     this.tenderData = [];
@@ -307,7 +533,7 @@ export class CostingApprovalComponent {
     var reqTend = {
       requeststatus_id: this.form.value.requeststatus_id = '7003', // Updated condition
       // requeststatus_id: (this.reqList[0].requeststatus_id == '7003') ? '7003' : '', // Updated condition
-      tender_id : this.tenderID,
+      tender_id: this.tenderID,
       working_notes: this.form.value.working_notes,
     };
     this.apiService.createApproval(reqTend).subscribe((res: any) => {
@@ -333,8 +559,8 @@ export class CostingApprovalComponent {
       var reqTend = {
 
         working_notes: this.form.value.working_notes,
-        requeststatus_id:this.form.value.tenderstatus_id,
-        tender_id : this.tenderID,
+        requeststatus_id: this.form.value.tenderstatus_id,
+        tender_id: this.tenderID,
       }
       this.apiService.createApproval(reqTend).subscribe((res: any) => {
         let response: any = res;
